ef663f6db284171adc5c18d37e419bd3f8d3c76a
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-07-07 20:17:30 +0900

    bottom appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok/screens/sign_up/sign_up_screen.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -12,11 +13,12 @@ class TikTokApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // 틱톡 시그니처 색상
         primaryColor: Color(0xFFE9435A),
       ),
-      home: Container(),
+      home: SignUpScreen(),
     );
   }
 }
