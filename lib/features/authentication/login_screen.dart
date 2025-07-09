c5eeb0661bdfa8382da505ac5dfadaebb7caf1e5
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2025-07-09 22:08:13 +0900

    login screen

--- lib/features/authentication/login_screen.dart
+++ lib/features/authentication/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok/constants/gaps.dart';
 import 'package:tiktok/constants/sizes.dart';
+import 'package:tiktok/features/authentication/login_form_screen.dart';
 import 'package:tiktok/features/authentication/widgets/auth_button.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -9,6 +10,12 @@ class LoginScreen extends StatelessWidget {
 
   void _onSignUpTap(BuildContext context) {
     Navigator.of(context).pop();
+  }
+
+  void _onEmailLoginTap(BuildContext context) {
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (context) => LoginFormScreen()));
   }
 
   @override
@@ -34,15 +41,17 @@ class LoginScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
               ),
               Gaps.v40,
-              // AuthButton(
-              //   icon: FaIcon(FontAwesomeIcons.user),
-              //   text: 'Use email & password',
-              // ),
+              AuthButton(
+                onTap: () => _onEmailLoginTap(context),
+                icon: FaIcon(FontAwesomeIcons.user),
+                text: 'Use email & password',
+              ),
               Gaps.v16,
-              // AuthButton(
-              //   icon: FaIcon(FontAwesomeIcons.apple),
-              //   text: 'Continue with Apple',
-              // ),
+              AuthButton(
+                onTap: () {},
+                icon: FaIcon(FontAwesomeIcons.apple),
+                text: 'Continue with Apple',
+              ),
             ],
           ),
         ),
